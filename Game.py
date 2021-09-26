0994e78e49a7d0c764099831656dd95c1d9a1fb3
jm-avila <jm.avila1309@icloud.com>
2021-09-26 13:39:58 +0200

    Game logic working

--- Game.py
+++ Game.py
@@ -1,26 +1,33 @@
 class Game:
     name = "unknown"
+    ai_name = "Mr Robot"
 
-    def greet(self):
-        self.handleNameInput()
-        print("Hello {}".format(self.name))
-        print("Please answer with a yes or no to go to the next question.")
-        understood = self.handleYesNoInput("Understood?")
-        if (understood):
-            print("Great")
-        else:
-            print("Better catch up")
-        print("Let's Start!!!")
-
-    def handleNameInput(self):
-        print("Whats your name?")
+    def handleStrInput(self, question):
+        print(question)
         userInput = input()
         if (len(userInput) == 0):
-            return self.name
-        self.name = userInput
+            return self.handleStrInput(question)
+        return userInput
 
-    def handleYesNoInput(self, question):
+    def handleNumInput(self, question):
         print(question)
+        int = "Input a number from 1 - 10"
+        print(int)
+        userInput = input()
+        if (len(userInput) == 0):
+            userInput = self.handleNumInput(question)
+        try:
+            userInput = float(userInput)
+        except:
+            print("Wrong input, try again.")
+            print(int)
+            userInput = self.handleNumInput(question)
+        if(userInput < 0 or userInput > 10):
+            userInput = self.handleNumInput(question)
+        return userInput
+
+    def handleBoolInput(self, question, first = False):
+        print(question, "yes/no")
         userInput = input()
         userInput = userInput.lower()
         if (userInput == "yes"):
@@ -29,6 +36,57 @@ class Game:
             return False
         if (len(userInput) == 0):
             print("No input was provided!!!")
+        return self.handleBoolInput(question)
+
+    def greet(self):
+        name = self.handleStrInput("Whats your name?")
+        self.name = name
+        print("Hello {}".format(name))
+        print("Please answer with a yes or a no.")
+        understood = self.handleBoolInput("Understood?")
+        if (understood):
+            print("Great")
         else:
-            print("Answer with a yes or no")
-        return self.handleYesNoInput()
+            print("Better catch up")
+        print("Let's Start!!!")
+        self.question1()
+
+    def question1(self):
+        ans = self.handleBoolInput("Do you know me?")
+        if (ans):
+            self.sayMyName()
+        else:
+            self.wannaGuess()
+
+    def sayMyName(self):
+        ans = self.handleStrInput("Say my name")
+        self.ending1(ans)
+
+    def wannaGuess(self):
+        ans = self.handleBoolInput("Wanna guess?")
+        if (ans):
+            self.guessLen()
+        else:
+            self.ending3(False)
+
+    def guessLen(self):
+        ans = self.handleNumInput("How many letters are in may name?")
+        self.ending2(ans)
+
+    def ending1(self, guess):
+        if(guess.lower() == self.ai_name.lower()):
+            print("You're right I'am: {}".format(self.ai_name))
+        else:
+            print("Never heard of {}".format(guess))
+
+    def ending2(self, num):
+        if (num == len(self.ai_name)):
+            print("You're right, My name is: {}".format(self.ai_name))
+        else:
+            print("Sorry, try again.")
+
+    def ending3(self, bool):
+        if (bool):
+            print("My name is: {}".format(self.ai_name))
+        else:
+            print("I guess you'll never know.")
